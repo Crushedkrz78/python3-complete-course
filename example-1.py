a5e4916d0262607735865770ea0f5b0c8329f4b8
Crushedkrz78 <crispmolinito93@gmail.com>
2022-07-07 12:24:12 -0500

    Segunda prueba del Hola Mundo

--- example-1.py
+++ example-1.py
@@ -3,3 +3,5 @@ print("Hola Mundo")
 #En este ejemplo renovado, se estará trabajando en un ambiente Linux virtualizado para no contaminar el ambiente local del equipo
 
 print("Hola Mundo desde Ubuntu 22.04")
+
+print("Segunda prueba del Hola Mundo en Ubuntu")
